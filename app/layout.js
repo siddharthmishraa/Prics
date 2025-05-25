0ebb233b546c91495cb51dd6b8791d1d244ba23a
Priya <priyayelisetty25@gmail.com>
2025-05-26 03:18:52 +0530

    changed layout.js
    changed layout.js to make it usable

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers"; // Import the wrapper
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,10 +20,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Providers>
+          {/* <Navbar /> */}
+          {children}
+        </Providers>
       </body>
     </html>
   );
